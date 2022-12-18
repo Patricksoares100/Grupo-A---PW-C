e970d39d4d05a9e4b23149b52762baf714fc3826
Patrick Soares 2220905 <2220905@my.ipleiria.pt>
2022-12-18 19:05:17 +0000

    background-repeat: initial;ackground-size: cover; 	background-repeat: initial;lease enter the commit message for your changes. Lines starting

--- js/javascript.js
+++ js/javascript.js
@@ -9,16 +9,14 @@ function search() {
 })
 }
 
-
+//exemplo dado na aula de rodar texto corrido
 function animate_string(id) {
     var element = document.getElementById(id);
-    // Returns a reference to the element by its ID; the ID is a string which can be used to identify the element; it can be established using the id attribute in HTML, or from script.
-    var textNode = element.childNodes[0]; // will produce the same result as the HTML content of the first child node.
+    var textNode = element.childNodes[0]; 
     var text = textNode.data;
-
     setInterval(function () {
-     text = text[text.length - 1] + text.substring(0, text.length - 1); 
-     // The length property represents the length of a string. It returns the number of characters units in the string.
+     text = text[text.length - 1] + text.substring(0, text.length - 1);      
       textNode.data = text;
     }, 100);
 }
+
